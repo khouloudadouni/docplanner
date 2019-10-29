9862b5c69dba1c07dd82cd361d3405864419692b
khouloudadouni <khouloudadouni1998@gmail.com>
2019-10-29 11:18:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from  './Components/header';
+import Main from './Components/Main';
+import Mainn from './Components/Mainn';
+import Mainnn from './Components/Mainnn';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Mainn/>
+      <Mainnn/>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
